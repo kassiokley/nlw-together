748f611c72ed4034fbe965c67f1ffc3eecb86044
kassiokley <seloseishiro@gmail.com>
2021-06-22 02:10:09 -0300

    Adiciona className e id do scss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,24 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss'
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="" />
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire dúvidas da sua audência em tempo real</p>
       </aside>
       <main>
-        <div>
-          <img src={logoImg} alt="" />
-          <button>
-            <img src={googleIconImg} alt="" />
+        <div className="main-content">
+          <img src={logoImg} alt="Logo do Letmeask" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Logo da Google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input 
               type="text"
