ae3745e90831efa40c7dcea5678505b7b9cd5b22
kassiokley <seloseishiro@gmail.com>
2021-06-22 02:53:28 -0300

    Adiciona navegacao entre pages

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,7 +1,9 @@
-import { Button } from '../components/Button'
+import { Link } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
+
+import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
 
@@ -27,7 +29,7 @@ export function NewRoom() {
             </Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
