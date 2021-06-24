9c024feb50ef43e906d1ed9c1ffc32d1cb50d0ec
kassiokley <seloseishiro@gmail.com>
2021-06-24 18:41:29 -0300

    Adiciona propriedade children

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react'
+
 import '../styles/question.scss'
 
 type QuestionProps = {
@@ -6,13 +8,15 @@ type QuestionProps = {
     name: string,
     avatar: string
   },
+  children?: ReactNode,
   //isHighlighted: boolean,
   //isAnswered: boolean
 }
 
 export function Question({
   content,
-  author
+  author,
+  children
 }: QuestionProps) {
   return (
     <div className="question">
@@ -22,7 +26,7 @@ export function Question({
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>{children}</div>
       </footer>
     </div>
   )
