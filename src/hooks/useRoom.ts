98f6db3b90566027013df74e9726b2273c25fdaf
kassiokley <seloseishiro@gmail.com>
2021-06-24 23:03:39 -0300

    Refatora codigo do hook useRoom

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,32 +1,40 @@
 import { useState, useEffect } from 'react'
 
 import { database } from '../services/firebase'
+import { useAuth } from './useAuth'
 
 type QuestionType = {
-  id: string;
+  id: string,
   author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isHighlighted: boolean
-  isAnswered: boolean
+    name: string,
+    avatar: string,
+  },
+  content: string,
+  isHighlighted: boolean,
+  isAnswered: boolean,
+  likeCount: number,
+  likeId: string | undefined,
 }
 
 type FirebaseQuestions = Record<
   string,
   {
     author: {
-      name: string;
-      avatar: string;
-    };
-    content: string;
-    isHighlighted: boolean;
-    isAnswered: boolean;
+      name: string,
+      avatar: string,
+    },
+    content: string,
+    isHighlighted: boolean,
+    isAnswered: boolean,
+    likes: Record<string, {
+      authorId: string,
+    }>
   }
 >
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth()
+
   const [questions, setQuestions] = useState<QuestionType[]>([])
 
   const [title, setTitle] = useState("")
@@ -47,6 +55,8 @@ export function useRoom(roomId: string) {
             author: value.author,
             isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
           }
         }
       )
@@ -55,7 +65,11 @@ export function useRoom(roomId: string) {
 
       setQuestions(parsedQuestions)
     })
-  }, [roomId])
+
+    return () => {
+      roomRef.off('value')
+    }
+  }, [roomId, user?.id])
 
   return {
     questions,
